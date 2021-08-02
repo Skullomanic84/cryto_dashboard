c297b00919717b682b0972014e593f7a05b16174
Skullomanic84 <jhshiloh@gmail.com>
2021-08-02 12:42:52 +0200

    Added AppBar and grid

--- src/Apps/index.js
+++ src/Apps/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 
 function App() {
   return (
     
     <AppLayout>
-    hello world
+        <AppBar />
     </AppLayout>
   );
 }
