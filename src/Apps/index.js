bc040b1a15d270317e69646ea267319d43aef648
Skullomanic84 <jhshiloh@gmail.com>
2021-08-02 12:24:25 +0200

    Added App layout

--- src/Apps/index.js
+++ src/Apps/index.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
+import AppLayout from './AppLayout';
 
 function App() {
   return (
     
-    <div>
-    hello world</div>
+    <AppLayout>
+    hello world
+    </AppLayout>
   );
 }
 
